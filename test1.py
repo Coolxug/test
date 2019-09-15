ba00162ff5e6986720d8cfc30df4d7257a897c73
hahaha <45685662+aaha-star@users.noreply.github.com>
2019-09-15 20:44:39 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
+
+language: python
+python:
+  - "2.7"
+# command to install dependencies
+install:
+    - pip install flake8 flake8-respect-noqa pre-commit
+# command to run tests
+script:
+    - make tes
 print("hello")
